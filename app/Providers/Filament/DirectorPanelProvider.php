0b7ddd1424ccb1f5b0289a9b57254aae19606bcb
ferntzy <rolaniog@gmail.com>
2026-02-18 15:18:36 +0800

    added middleware and panel providers

---
+++ app/Providers/Filament/DirectorPanelProvider.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Providers\Filament;
+
+use Filament\Http\Middleware\Authenticate;
+use Filament\Http\Middleware\AuthenticateSession;
+use Filament\Http\Middleware\DisableBladeIconComponents;
+use Filament\Http\Middleware\DispatchServingFilamentEvent;
+use Filament\Pages\Dashboard;
+use Filament\Panel;
+use Filament\PanelProvider;
+use Filament\Support\Colors\Color;
+use Filament\Widgets\AccountWidget;
+use Filament\Widgets\FilamentInfoWidget;
+use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Illuminate\Cookie\Middleware\EncryptCookies;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
+use Illuminate\Routing\Middleware\SubstituteBindings;
+use Illuminate\Session\Middleware\StartSession;
+use Illuminate\View\Middleware\ShareErrorsFromSession;
+
+class DirectorPanelProvider extends PanelProvider
+{
+    public function panel(Panel $panel): Panel
+    {
+        return $panel
+            ->id('director')
+            ->path('director')
+            ->colors([
+                'primary' => Color::Blue,
+            ])
+            ->discoverResources(in: app_path('Filament/Director/Resources'), for: 'App\Filament\Director\Resources')
+            ->discoverPages(in: app_path('Filament/Director/Pages'), for: 'App\Filament\Director\Pages')
+            ->pages([
+                Dashboard::class,
+            ])
+            ->discoverWidgets(in: app_path('Filament/Director/Widgets'), for: 'App\Filament\Director\Widgets')
+            ->widgets([
+                AccountWidget::class,
+                FilamentInfoWidget::class,
+            ])
+            ->middleware([
+                EncryptCookies::class,
+                AddQueuedCookiesToResponse::class,
+                StartSession::class,
+                AuthenticateSession::class,
+                ShareErrorsFromSession::class,
+                VerifyCsrfToken::class,
+                SubstituteBindings::class,
+                DisableBladeIconComponents::class,
+                DispatchServingFilamentEvent::class,
+            ])
+            ->authMiddleware([
+                Authenticate::class,
+                'role:director',
+            ])
+            ->tenantMiddleware([
+           ], isPersistent: true);
+    }
+}
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,4 +3,8 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\DirectorPanelProvider::class,
+    App\Providers\Filament\DoctorPanelProvider::class,
+    App\Providers\Filament\NursePanelProvider::class,
+    App\Providers\Filament\PatientPanelProvider::class,
 ];
