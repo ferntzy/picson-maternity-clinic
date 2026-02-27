85c404a65537d611319c7a1cc40c3e8e608c15bf
Elijah Batestil <elijahbatestil26@gmail.com>
2026-02-27 22:22:52 +0800

    Added api for fetch patient informations for mobile app

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,12 @@
 <?php
 
 use App\Http\Controllers\MobileAuth\Auth;
+use App\Http\Controllers\Profiles\PatientProfile;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [Auth::class, 'login']);
+
+Route::prefix('patient-profiles')->group(function(){
+    Route::get('/', [PatientProfile::class, 'index']);
+    Route::post('/patient-information', [PatientProfile::class, 'getPatientInformation']);
+});
