3456202b55e003d26f365aacb7e0e1793c94a18b
Elijah Batestil <elijahbatestil26@gmail.com>
2026-03-02 18:36:32 +0800

    Fixed login responses

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,10 +14,32 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create an admin profile
+        $profile = Profiles::create([
+            'firstname' => 'Rovic',
+            'middlename' => 'Bayot',
+            'lastname' => 'Jud',
+            'address' => 'Inopacan, Leyte',
+            'sex' => 'Female',
+            'birth_place' => 'Maasin City, Southern Leyte',
+            'civil_status' => 'single',
+            'religion' => 'Muslim',
+            'nationality' => 'Australian',
+            'birth_date' => '1998-02-20',
+            'emergency_contact_name' => 'Rafael T. Virgin',
+            'emergency_contact_number' => '09287645377',
+            'philhealth_number' => '01983265123',
+            'blood_type' => 'A+',
+            'allergies' => 'None',
+            'contact_num' => '092837467577',
+            'role' => 'admin',
+        ]);
+
+        // Attach the profile to the user
         User::create([
             'email' => 'admin@example.com',
             'password' => bcrypt('123456'),
-            'role' => 'admin',
+            'profile_id' => $profile->id,
         ]);
     }
 }
